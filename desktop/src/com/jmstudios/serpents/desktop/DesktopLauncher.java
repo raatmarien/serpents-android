70873b924f031d8a3095063282585993651fc1e3
Marien Raat <marienraat@riseup.net>
2016-08-03 14:44:53 +0200

    Set window settings in DesktopLauncher

--- desktop/src/com/jmstudios/serpents/desktop/DesktopLauncher.java
+++ desktop/src/com/jmstudios/serpents/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.jmstudios.serpents.SerpentsGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SerpentsGame(), config);
+                config.title = "Serpents";
+                config.width = 800;
+                config.height = 480;
+                new LwjglApplication(new SerpentsGame(), config);
 	}
 }
