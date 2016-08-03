c652f1143ea3c570d420c8d660d61ed05c0c56cd
Marien Raat <marienraat@riseup.net>
2016-08-03 13:48:29 +0200

    Initial commit

---
+++ desktop/src/com/jmstudios/serpents/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jmstudios.serpents.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jmstudios.serpents.SerpentsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SerpentsGame(), config);
+	}
+}
